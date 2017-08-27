ae8f3f91e606a81b99da4ffe289b17cc7de72fdc
uiscias <semias@gmail.com>
2017-08-27 19:10:41 +0200

    Add and manage Reminder for Vaccination

---
+++ src/AppBundle/Form/PasswordType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PasswordType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('plainPassword')
+                ->add('password')
+                ->add('passwordConfirm')
+                ->add('Sauver', SubmitType::class, array(
+                'attr' => array('class' => 'button btn-appoint')));
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_user';
+    }
+
+
+}
